6ee961a02d570fbff7485b6b5a80ad0f1e40377b
ManuelLunaPerez <manuellunaperez@gmail.com>
2014-03-13 10:52:11 +0100

    Falta mensaje de error

---
+++ programatussam.py
@@ -0,0 +1,25 @@
+#encoding=utf-8
+import requests
+from lxml import etree
+from suds.client import Client
+import os
+
+linea = int(raw_input("Introduce una linea de Tussam: "))
+
+cliente = Client('http://www.infobustussam.com:9001/services/dinamica.asmx?wsdl', retxml=True)
+
+respuesta = cliente.service.GetStatusLinea(linea)
+raiz =  etree.fromstring(respuesta.encode("utf-8"))
+raiz2 = raiz[0][0]
+ns = "{http://tempuri.org/}"
+xml = raiz2.find(ns+"GetStatusLineaResult")
+activos = xml.find(ns+"activos")
+frecuencia = xml.find(ns+"frec_bien")
+graves = xml.find(ns+"graves")
+
+os.system('clear')
+print "Información sobre la linea de Tussam: %s" % linea
+print "Número de coches actual : %s" % activos.text
+print "Número de coches que van bien de frecuencia: %s" % frecuencia.text
+print "Número de incidencias graves: %s" % graves.text
+
